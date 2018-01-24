9ff20ad1c4abff3d6e615bfb6466a4eccd046f50
angrypig7 <angrypig777@gmail.com>
2018-01-24 17:58:08 +0900

    added NRF24_getsettings

--- NRF24L01/NRF24_2_Receive/NRF24_2_Receive.ino
+++ NRF24L01/NRF24_2_Receive/NRF24_2_Receive.ino
@@ -3,21 +3,23 @@
 #include <RF24.h>
 RF24 radio(7, 8);
 const byte rxAddr[6] = "00001";
-int text = 0;
+String text = "";
 
 void setup(){
   Serial.begin(9600);
   
   radio.begin();
   radio.openReadingPipe(0, rxAddr);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setChannel(108);
   radio.startListening();
 }
 
 void loop(){
   if (radio.available()){
-    text = 0;
+    text = "";
     radio.read(&text, sizeof(text));
     Serial.println(text);
   }
-  delay(100);
 }
---
+++ NRF24L01/NRF24_GetSettings/NRF24_GetSettings.ino
@@ -0,0 +1,25 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+//#include <LowPower.h>  //AVR Low Power library that will basically turn this arduino off
+RF24 radio(7, 8);
+
+void setup(){
+  Serial.begin(115200);
+  radio.begin();
+
+  Serial.print("Payload Size : ");
+  Serial.println(radio.getPayloadSize());  //The number of bytes in the payload
+  Serial.print("RF Channel : ");
+  Serial.println(radio.getChannel());  //The currently configured RF Channel
+  Serial.print("PA Level : ");
+  Serial.println(radio.getPALevel());  //NRF24L01: -18dBm, -12dBm, -6dBm and 0dBm
+  Serial.print("Datarate : ");
+  Serial.println(radio.getDataRate());  //The value is one of 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS
+
+  delay(500);
+}
+
+void loop(){
+//  LowPower.idle(SLEEP_FOREVER, ADC_OFF, TIMER2_OFF,TIMER1_OFF, TIMER0_OFF,SPI_OFF, USART0_OFF,TWI_OFF );
+}
