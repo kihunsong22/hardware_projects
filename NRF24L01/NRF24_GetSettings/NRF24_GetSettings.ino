3f99232448ef57994401d790de0e9758ffb9246d
angrypig7 <angrypig777@gmail.com>
2018-01-24 18:03:57 +0900

    improved NRF24getsettings

--- NRF24L01/NRF24_GetSettings/NRF24_GetSettings.ino
+++ NRF24L01/NRF24_GetSettings/NRF24_GetSettings.ino
@@ -4,9 +4,17 @@
 //#include <LowPower.h>  //AVR Low Power library that will basically turn this arduino off
 RF24 radio(7, 8);
 
+int serial_putchar(char c, FILE* f) {
+   if (c == '\n') serial_putchar('\r', f);
+   return Serial.write(c) == 1? 0 : 1;
+}
+FILE serial_stdout;
+
 void setup(){
   Serial.begin(115200);
   radio.begin();
+  fdev_setup_stream(&serial_stdout, serial_putchar, NULL, _FDEV_SETUP_WRITE);
+  stdout = &serial_stdout;
 
   Serial.print("Payload Size : ");
   Serial.println(radio.getPayloadSize());  //The number of bytes in the payload
@@ -16,6 +24,9 @@ void setup(){
   Serial.println(radio.getPALevel());  //NRF24L01: -18dBm, -12dBm, -6dBm and 0dBm
   Serial.print("Datarate : ");
   Serial.println(radio.getDataRate());  //The value is one of 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS
+  Serial.println();Serial.println();
+  Serial.println("RF24 Detailed Info : ");
+  radio.printDetails();
 
   delay(500);
 }
