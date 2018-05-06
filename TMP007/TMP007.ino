94181c8982d74e0e834f7fbfb0c0557a2db1aa90
angrypig7 <angrypig777@gmail.com>
2018-05-06 18:17:11 +0900

    TMP update

--- TMP007/TMP007.ino
+++ TMP007/TMP007.ino
@@ -1,24 +1,21 @@
 #include <Wire.h>
 #include "Adafruit_TMP007.h"
 
-// Connect VCC to +3V (its a quieter supply than the 5V supply on an Arduino
-// Gnd -> Gnd
-// SCL connects to the I2C clock pin. On newer boards this is labeled with SCL
-// otherwise, on the Uno, this is A5 on the Mega it is 21 and on the Leonardo/Micro digital 3
-// SDA connects to the I2C data pin. On newer boards this is labeled with SDA
-// otherwise, on the Uno, this is A4 on the Mega it is 20 and on the Leonardo/Micro digital 2
+// 3.3V 사용이 더 안정적일수도 있다고 하는데 모르겠다
+// SCL(A5), SDA(A4)
 
 Adafruit_TMP007 tmp007;
 //Adafruit_TMP007 tmp007(0x41);  // start with a diferent i2c address!
 
 void setup() { 
   Serial.begin(115200);
-  Serial.println("Adafruit TMP007 example");
+  Serial.println("Adafruit TMP007");
 
-  // you can also use tmp007.begin(TMP007_CFG_1SAMPLE) or 2SAMPLE/4SAMPLE/8SAMPLE
+  // 1 sample takes 250milsec
+  // tmp007.begin(TMP007_CFG_1SAMPLE) /2/4/8 - 16 by default
   if (! tmp007.begin(TMP007_CFG_2SAMPLE)) {
     Serial.println("No sensor found");
-    while (1);
+    continue;
   }
 }
 
@@ -29,5 +26,5 @@ void loop() {
    Serial.print("   DIE temp: "); Serial.print(diet); Serial.println("*C");
    Serial.println();
    
-   delay(500); // 250mil for 1 sample
+   delay(500); // 250mil for every sampling rate
 }
