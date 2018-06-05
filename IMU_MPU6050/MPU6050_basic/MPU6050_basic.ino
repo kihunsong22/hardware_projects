8ffae3db6fdfdb76f634717dbda887f952080250
angrypig7 <angrypig777@gmail.com>
2018-06-06 08:37:23 +0900

    general update with LoRa

---
+++ IMU_MPU6050/MPU6050_basic/MPU6050_basic.ino
@@ -0,0 +1,41 @@
+#include<Wire.h>
+
+const int MPU=0x68;  //MPU 6050 의 I2C 기본 주소
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+void setup(){
+  Wire.begin();      //Wire 라이브러리 초기화
+  Wire.beginTransmission(MPU); //MPU로 데이터 전송 시작
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     //MPU-6050 시작 모드로
+  Wire.endTransmission(true); 
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+}
+
+void loop(){
+  Wire.beginTransmission(MPU);    //데이터 전송시작
+  Wire.write(0x3B);               // register 0x3B (ACCEL_XOUT_H), 큐에 데이터 기록
+  Wire.endTransmission(false);    //연결유지
+  Wire.requestFrom(MPU,14,true);  //MPU에 데이터 요청
+  //데이터 한 바이트 씩 읽어서 반환
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  
+  //시리얼 모니터에 출력
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+  Serial.println();
+  delay(200);
+}
---
+++ LoRa/UM420 SX1278/receive/receive.ino
@@ -0,0 +1,26 @@
+//Woon Jun Shen
+//UM402 (433 MHz UART)
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(2, 3); //TX, RX
+// gnd SET_A and SET_B for Normal Mode (Send and Receive)
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+}
+
+void loop() {
+  
+  
+  if(Serial.available() > 0){//Read from serial monitor and send over UM402
+    String input = Serial.readString();
+    mySerial.println(input);    
+  }
+ 
+  if(mySerial.available() > 1){//Read from UM402 and send to serial monitor
+    String input = mySerial.readString();
+    Serial.println(input);    
+  }
+  delay(20);
+}
