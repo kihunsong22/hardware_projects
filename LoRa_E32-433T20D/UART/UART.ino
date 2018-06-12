b68a73070b273198475ef56115eeb76c6034e770
angrypig7 <angrypig777@gmail.com>
2018-06-12 22:57:04 +0900

    .

--- LoRa_E32-433T20D/UART/UART.ino
+++ LoRa_E32-433T20D/UART/UART.ino
@@ -1,10 +1,6 @@
 #include <SoftwareSerial.h>
-#define MODE 1
+#define MODE 1 //Operating Mode: 0~3
 SoftwareSerial lora(2, 3); //TX, RX
-
-//사용자 설정
-//Operating Mode: 0~3
-//
 
 int prevauxstate = 0, auxstate = 0, auxcount = 0;
 int modeArr[4][2] = {{0, 0}, {1, 0}, {0, 1}, {1, 1}};
@@ -14,10 +10,9 @@ int M1 = 4;
 int AUX = 7;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   lora.begin(9600);
 
-  Serial.println();
   Serial.println("Power on");
 
   pinMode(M0, OUTPUT);
@@ -30,29 +25,37 @@ void setup() {
   Serial.println(modeArr[MODE][0]);
   Serial.print("M1: ");
   Serial.println(modeArr[MODE][1]);
+  Serial.println();
 }
 
 void loop() {
-  prevauxstate = auxstate;
-  auxstate = digitalRead(AUX);
-  if(prevauxstate != auxstate){
-    auxcount++;
-  }
-  else if(auxcount>0){
-    Serial.print("auxSTATE : ");
-    Serial.print(auxstate);
-    Serial.print("        auxcount : ");
-    Serial.println(auxcount);
-    auxcount = 0;
-  }
+  // prevauxstate = auxstate;
+  // auxstate = digitalRead(AUX);
+  // if(prevauxstate != auxstate){
+  //   auxcount++;
+  // }
+  // else if(auxcount>0){
+  //   Serial.print("auxSTATE : ");
+  //   Serial.print(auxstate);
+  //   Serial.print("        auxcount : ");
+  //   Serial.println(auxcount);
+  //   auxcount = 0;
+  // }
 
-  if(Serial.available() > 0){//사용자 입력을 LoRa모듈에 전송
-    String input = Serial.readString();
-    lora.println(input);
-  }
+  // if(Serial.available() > 0){//사용자 입력을 LoRa모듈에 전송
+  //   String input = Serial.readString();
+  //   lora.println(input);
+  // }
  
-  if(lora.available() > 1){//LoRa모듈의 출력을 시리얼 모니터에 출력
-    String input = lora.readString();
-    Serial.println(input);
+  // if(lora.available() > 1){//LoRa모듈의 출력을 시리얼 모니터에 출력
+  //   String input = lora.readString();
+  //   Serial.println(input);
+  // }
+
+  if (lora.available()) {
+    Serial.write(lora.read());
+  }
+  if (Serial.available()) {
+    lora.write(Serial.read());
   }
 }
