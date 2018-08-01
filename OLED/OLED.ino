53693a201f7e93b3a36cdf34487bd4556be25a8c
angrypig7 <angrypig777@gmail.com>
2018-08-01 15:31:34 +0900

    OLED

---
+++ OLED/OLED.ino
@@ -0,0 +1,15 @@
+#include "U8glib.h"
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);
+
+void setup() {
+}
+
+void loop() {
+    u8g.firstPage();
+    do{
+        u8g.setFont(u8g_font_unifont);
+        u8g.setPrintPos(0,10);
+        u8g.print("Hello, world");
+        u8g.drawStr(0,30, "Hello, world!");
+    }while(u8g.nextPage());
+}
