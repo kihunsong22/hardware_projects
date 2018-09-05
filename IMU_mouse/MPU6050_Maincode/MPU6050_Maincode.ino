f991cac4cfab359350819c9ea45e05bcdca17833
angrypig7 <angrypig777@gmail.com>
2018-09-05 22:19:12 +0900

    IMU mouse code update_final1

--- IMU_mouse/MPU6050_Maincode/MPU6050_Maincode.ino
+++ IMU_mouse/MPU6050_Maincode/MPU6050_Maincode.ino
@@ -1,4 +1,5 @@
 #include<Wire.h>
+#include<Mouse.h>
 #define MPU 0x68
 
 int16_t AcX, AcY, AcZ, offX=0, offY=0, offZ=0, calX, calY, calZ;
@@ -6,16 +7,17 @@ int32_t velX=0, velY=0;
 int16_t posX=0, posY=0;
 // int16_t Tmp,GyX,GyY,GyZ;
 
-float EMA_a = 0.06;
+float EMA_a = 0.06, mouse_sen=0.0006;
 int stableX=0, stableY=0;
+int buttonL=0, buttonR=0, bstateL=0, bstateR=0;  //button left, right
 
-void calibrate();
-int EMA_function(float alpha, int16_t latest, int16_t stored);
+void calibrate();  //calibrate initial offset
+int EMA_function(float alpha, int16_t latest, int16_t stored){
+    return round( alpha * latest) + round((1-alpha) * stored );
+}
 
 void setup(){
-    Serial.begin(115200);
-
-    // Wire.setClock(400000);
+    Wire.setClock(400000);
     Wire.begin();
     delay(10);
     Wire.beginTransmission(MPU);
@@ -23,14 +25,33 @@ void setup(){
     Wire.write(0x00);  // set 0x6B to zero -> wakes up the MPU
     Wire.endTransmission(true);
 
-    calibrate();  //calibrate offset
+    calibrate();
 }
 
 void loop(){
+    buttonL = digitalRead(5);
+    buttonR = digitalRead(6);
+    if(buttonL != bstateL){
+        if(buttonL){
+            Mouse.press(MOUSE_LEFT);
+        }else{
+            Mouse.release(MOUSE_LEFT);
+        }
+    }
+    if(buttonR != bstateR){
+        if(buttonR){
+            Mouse.press(MOUSE_RIGHT);
+        }else{
+            Mouse.release(MOUSE_RIGHT);
+        }
+    }
+    bstateL = buttonL;
+    bstateR = buttonR;
+
     Wire.beginTransmission(MPU);
-    Wire.write(0x3B);  // register 0x3B (ACCEL_XOUT_H), 큐에 데이터 기록
+    Wire.write(0x3B);  // register 0x3B (ACCEL_XOUT_H), save data to queue
     Wire.endTransmission(false);
-    Wire.requestFrom(MPU,14,true);  //request data
+    Wire.requestFrom(MPU,14,true);  //request data from MPU
 
     AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
     AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
@@ -40,7 +61,7 @@ void loop(){
     // calX = EMA_function(EMA_a, AcX, calX);
     // calY = EMA_function(EMA_a, AcY, calY);
 
-    if(AcX>600 || AcX<-600){
+    if(AcX>600 || AcX<-600){  //X acceleration
         velX += AcX;
         stableX=0;
     }else{
@@ -48,7 +69,7 @@ void loop(){
     }
     velX = stableX>5 ? (velX/1.5) : velX;
     
-    if(AcY>600 || AcY<-600){
+    if(AcY>600 || AcY<-600){  //Y acceleration
         velY += AcY;
         stableY=0;
     }else{
@@ -64,14 +85,15 @@ void loop(){
     // Serial.print(calX); Serial.print(" ");
     // Serial.print(calY); Serial.print(" ");
     
-    Serial.print(velX); Serial.print(" ");
-    Serial.print(velY); Serial.print(" ");
+    // Serial.print(velX); Serial.print(" ");
+    // Serial.print(velY); Serial.print(" ");
+    // Serial.print(posX); Serial.print(" ");
+    // Serial.print(posY); Serial.print(" ");
+    // Serial.println();
 
-    Serial.print(posX); Serial.print(" ");
-    Serial.print(posY); Serial.print(" ");
+    Mouse.move(-velX*mouse_sen, velY*mouse_sen, 0);
 
-    Serial.println();
-    delay(6);
+    delay(4);
 }
 
 void calibrate(){
@@ -91,15 +113,22 @@ void calibrate(){
     offY /= 20;
     offZ /= 20;
 
-    Serial.println("==========================================");
-    Serial.print(offX); Serial.print(" ");
-    Serial.print(offY); Serial.print(" ");
-    Serial.print(offZ); Serial.println(" ");
-    Serial.println("==========================================");
+    // Serial.println("==========================================");  //Offset data
+    // Serial.print(offX); Serial.print(" ");
+    // Serial.print(offY); Serial.print(" ");
+    // Serial.print(offZ); Serial.println(" ");
+    // Serial.println("==========================================");
 
-    delay(5);
-}
-
-int EMA_function(float alpha, int16_t latest, int16_t stored){
-    return round( alpha * latest) + round((1-alpha) * stored );
+    RXLED0;
+    TXLED0;
+    for(int i=0; i<10; i++){
+        TXLED1;
+        RXLED0;
+        delay(80);
+        TXLED0;
+        RXLED1;
+        delay(80);
+    }
+    RXLED0;
+    TXLED0;
 }
