99a964ccadf1c627766027f931b1d51f7584224a
angrypig7 <angrypig777@gmail.com>
2018-12-15 14:32:28 +0900

    error correciton

--- _MISC/_Sketchbook/libraries/Adafruit_GFX_Library/Adafruit_SPITFT.cpp
+++ _MISC/_Sketchbook/libraries/Adafruit_GFX_Library/Adafruit_SPITFT.cpp
@@ -43,8 +43,26 @@
 #endif
 #include <limits.h>
 
+#ifdef PORT_IOBUS
+// On SAMD21, redefine digitalPinToPort() to use the slightly-faster
+// PORT_IOBUS rather than PORT (not needed on SAMD51).
+#undef  digitalPinToPort
+#define digitalPinToPort(P) (&(PORT_IOBUS->Group[g_APinDescription[P].ulPort]))
+#endif
+
 #include "Adafruit_SPITFT_Macros.h"
 
+#ifdef USE_SPI_DMA
+#include <Adafruit_ZeroDMA.h>
+#include <malloc.h> // memalign() function
+
+// DMA transfer-in-progress indicator and callback
+static volatile boolean dma_busy = false;
+static void dma_callback(Adafruit_ZeroDMA *dma) {
+    dma_busy = false;
+}
+
+#endif // USE_SPI_DMA
 
 /**************************************************************************/
 /*!
@@ -172,6 +190,7 @@ Adafruit_SPITFT::Adafruit_SPITFT(uint16_t w, uint16_t h, SPIClass *spiClass,
 #endif
 }
 
+
 /**************************************************************************/
 /*!
     @brief   Initialiaze the SPI interface (hardware or software)
@@ -213,6 +232,122 @@ void Adafruit_SPITFT::initSPI(uint32_t freq) {
         digitalWrite(_rst, HIGH);
         delay(200);
     }
+
+#ifdef USE_SPI_DMA
+
+    // INITIALIZE DMA
+
+    if(dma.allocate() == DMA_STATUS_OK) { // Allocate channel
+        // The DMA library needs to allocate at least one valid descriptor,
+        // so we do that here. It's not used in the usual sense though,
+        // just before a transfer we copy descriptor[0] to this address.
+        if(dptr = dma.addDescriptor(NULL, NULL, 42, DMA_BEAT_SIZE_BYTE,
+          false, false)) {
+            // Allocate 2 scanlines worth of pixels on display's major axis,
+            // whichever that is, rounding each up to 2-pixel boundary.
+            int  major = (WIDTH > HEIGHT) ? WIDTH : HEIGHT;
+            major     += (major & 1); // -> next 2-pixel bound, if needed.
+            maxFillLen = major * 2;   // 2 scanlines
+            // Note to future self: if you decide to make the pixel buffer
+            // much larger, remember that DMA transfer descriptors can't
+            // exceed 65,535 bytes (not 65,536), meaning 32,767 pixels tops.
+            // Not that we have that kind of RAM to throw around right now.
+            if((pixelBuf[0] =
+              (uint16_t *)malloc(maxFillLen * sizeof(uint16_t)))) {
+                // Alloc OK. Get pointer to start of second scanline.
+                pixelBuf[1] = &pixelBuf[0][major];
+                // Determine number of DMA descriptors needed to cover
+                // entire screen when entire 2-line pixelBuf is used
+                // (round up for fractional last descriptor).
+                int numDescriptors = (WIDTH * HEIGHT + (maxFillLen - 1)) /
+                      maxFillLen;
+                // DMA descriptors MUST be 128-bit (16 byte) aligned.
+                // memalign() is considered 'obsolete' but it's replacements
+                // (aligned_alloc() or posix_memalign()) are not currently
+                // available in the version of ARM GCC in use, but this is,
+                // so here we are.
+                if((descriptor = (DmacDescriptor *)memalign(16,
+                  numDescriptors * sizeof(DmacDescriptor)))) {
+                    int                dmac_id;
+                    volatile uint32_t *data_reg;
+
+                    // THIS IS AN AFFRONT TO NATURE, but I don't know
+                    // any "clean" way to get the sercom number from the
+                    // SPIClass pointer (e.g. &SPI or &SPI1), which is
+                    // all we have to work with. SPIClass does contain
+                    // a SERCOM pointer but it is a PRIVATE member!
+                    // Doing an UNSPEAKABLY HORRIBLE THING here, directly
+                    // accessing the first 32-bit value in the SPIClass
+                    // structure, knowing that's (currently) where the
+                    // SERCOM pointer lives, but this ENTIRELY DEPENDS
+                    // on that structure not changing nor the compiler
+                    // rearranging things. Oh the humanity!
+
+                    if(*(SERCOM **)_spi == &sercom0) {
+                        dmac_id  = SERCOM0_DMAC_ID_TX;
+                        data_reg = &SERCOM0->SPI.DATA.reg;
+#if defined SERCOM1
+                    } else if(*(SERCOM **)_spi == &sercom1) {
+                        dmac_id  = SERCOM1_DMAC_ID_TX;
+                        data_reg = &SERCOM1->SPI.DATA.reg;
+#endif
+#if defined SERCOM2
+                    } else if(*(SERCOM **)_spi == &sercom2) {
+                        dmac_id  = SERCOM2_DMAC_ID_TX;
+                        data_reg = &SERCOM2->SPI.DATA.reg;
+#endif
+#if defined SERCOM3
+                    } else if(*(SERCOM **)_spi == &sercom3) {
+                        dmac_id  = SERCOM3_DMAC_ID_TX;
+                        data_reg = &SERCOM3->SPI.DATA.reg;
+#endif
+#if defined SERCOM4
+                    } else if(*(SERCOM **)_spi == &sercom4) {
+                        dmac_id  = SERCOM4_DMAC_ID_TX;
+                        data_reg = &SERCOM4->SPI.DATA.reg;
+#endif
+#if defined SERCOM5
+                    } else if(*(SERCOM **)_spi == &sercom5) {
+                        dmac_id  = SERCOM5_DMAC_ID_TX;
+                        data_reg = &SERCOM5->SPI.DATA.reg;
+#endif
+                    }
+
+                    dma.setPriority(DMA_PRIORITY_3);
+                    dma.setTrigger(dmac_id);
+                    dma.setAction(DMA_TRIGGER_ACTON_BEAT);
+
+                    // Initialize descriptor list.
+                    for(int d=0; d<numDescriptors; d++) {
+                        // No need to set SRCADDR, DESCADDR or BTCNT --
+                        // those are done in the pixel-writing functions.
+                        descriptor[d].BTCTRL.bit.VALID    = true;
+                        descriptor[d].BTCTRL.bit.EVOSEL   =
+                          DMA_EVENT_OUTPUT_DISABLE;
+                        descriptor[d].BTCTRL.bit.BLOCKACT =
+                           DMA_BLOCK_ACTION_NOACT;
+                        descriptor[d].BTCTRL.bit.BEATSIZE = DMA_BEAT_SIZE_BYTE;
+                        descriptor[d].BTCTRL.bit.DSTINC   = 0;
+                        descriptor[d].BTCTRL.bit.STEPSEL  = DMA_STEPSEL_SRC;
+                        descriptor[d].BTCTRL.bit.STEPSIZE =
+                          DMA_ADDRESS_INCREMENT_STEP_SIZE_1;
+                        descriptor[d].DSTADDR.reg         = (uint32_t)data_reg;
+                    }
+                    lastFillColor = 0x0000;
+                    lastFillLen   = 0;
+                    dma.setCallback(dma_callback);
+                    return; // Success!
+                }
+                // Else some alloc/init error along the way...clean up...
+                free(pixelBuf[0]);
+                pixelBuf[0] = pixelBuf[1] = NULL;
+            }
+            // Don't currently have a descriptor delete function in
+            // ZeroDMA lib, but if we did, it would be called here.
+        }
+        dma.free(); // Deallocate DMA channel
+    }
+#endif // end DMA init
 }
 
 /**************************************************************************/
@@ -311,61 +446,188 @@ void Adafruit_SPITFT::pushColor(uint16_t color) {
   endWrite();
 }
 
-
-
 /**************************************************************************/
 /*!
-    @brief   Blit multiple 2-byte colors  (must have a transaction in progress)
-    @param    colors Array of 16-bit 5-6-5 Colors to draw
-    @param    len  How many pixels to draw - 2 bytes per pixel!
+    @brief  Blit multiple 2-byte colors (must have a transaction in progress)
+    @param  colors  Array of 16-bit 5-6-5 Colors to draw
+    @param  len     How many pixels to draw - 2 bytes per pixel!
 */
 /**************************************************************************/
-void Adafruit_SPITFT::writePixels(uint16_t * colors, uint32_t len){
+void Adafruit_SPITFT::writePixels(uint16_t *colors, uint32_t len) {
+#ifdef USE_SPI_DMA
+    if(_sclk < 0) { // using hardware SPI?
+        int     maxSpan     = maxFillLen / 2; // One scanline max
+        uint8_t pixelBufIdx = 0;              // Active pixel buffer number
+        while(len) {
+            int count = (len < maxSpan) ? len : maxSpan;
+
+            // Because TFT and SAMD endianisms are different, must swap bytes
+            // from the 'colors' array passed into a DMA working buffer. This
+            // can take place while the prior DMA transfer is in progress,
+            // hence the need for two pixelBufs.
+            for(int i=0; i<count; i++) {
+                pixelBuf[pixelBufIdx][i] = __builtin_bswap16(*colors++);
+            }
+            // The transfers themselves are relatively small, so we don't
+            // need a long descriptor list. We just alternate between the
+            // first two, sharing pixelBufIdx for that purpose.
+            descriptor[pixelBufIdx].SRCADDR.reg       =
+              (uint32_t)pixelBuf[pixelBufIdx] + count * 2;
+            descriptor[pixelBufIdx].BTCTRL.bit.SRCINC = 1;
+            descriptor[pixelBufIdx].BTCNT.reg         = count * 2;
+            descriptor[pixelBufIdx].DESCADDR.reg      = 0;
+
+            while(dma_busy); // wait for prior line to complete
+
+            // Move new descriptor into place...
+            memcpy(dptr, &descriptor[pixelBufIdx], sizeof(DmacDescriptor));
+            dma_busy = true;
+            dma.startJob();                // Trigger SPI DMA transfer
+            pixelBufIdx = 1 - pixelBufIdx; // Swap DMA pixel buffers
+
+            len -= count;
+        }
+        lastFillColor = 0x0000; // pixelBuf has been sullied
+        lastFillLen   = 0;
+        while(dma_busy);        // Wait for last line to complete
+#ifdef __SAMD51__
+        _spi->setDataMode(SPI_MODE0); // See note in writeColor()
+#endif
+        return;
+    }
+#else
     SPI_WRITE_PIXELS((uint8_t*)colors , len * 2);
+#endif
 }
 
 /**************************************************************************/
 /*!
-    @brief   Blit a 2-byte color many times  (must have a transaction in progress)
-    @param    color  The 16-bit 5-6-5 Color to draw
-    @param    len    How many pixels to draw
+    @brief  Blit a 2-byte color many times (must have a transaction in progress)
+    @param  color  The 16-bit 5-6-5 Color to draw
+    @param  len    How many pixels to draw
 */
 /**************************************************************************/
-void Adafruit_SPITFT::writeColor(uint16_t color, uint32_t len){
-#ifdef SPI_HAS_WRITE_PIXELS
-    if(_sclk >= 0){
-        for (uint32_t t=0; t<len; t++){
-            writePixel(color);
-        }
-        return;
-    }
-    static uint16_t temp[SPI_MAX_PIXELS_AT_ONCE];
-    size_t blen = (len > SPI_MAX_PIXELS_AT_ONCE)?SPI_MAX_PIXELS_AT_ONCE:len;
-    uint16_t tlen = 0;
+void Adafruit_SPITFT::writeColor(uint16_t color, uint32_t len) {
 
-    for (uint32_t t=0; t<blen; t++){
-        temp[t] = color;
-    }
+    if(!len) return; // Avoid 0-byte transfers
 
-    while(len){
-        tlen = (len>blen)?blen:len;
-        writePixels(temp, tlen);
-        len -= tlen;
-    }
-#else
     uint8_t hi = color >> 8, lo = color;
-    if(_sclk < 0){ //AVR Optimization
-        for (uint32_t t=len; t; t--){
+
+    if(_sclk < 0) { // Using hardware SPI
+
+#ifdef USE_SPI_DMA
+
+        int i, d, numDescriptors;
+        if(hi == lo) { // If high & low bytes are same...
+            onePixelBuf = color;
+            // Can do this with a relatively short descriptor list,
+            // each transferring a max of 32,767 (not 32,768) pixels.
+            // This won't run off the end of the allocated descriptor list,
+            // since we're using much larger chunks per descriptor here.
+            numDescriptors = (len + 32766) / 32767;
+            for(d=0; d<numDescriptors; d++) {
+                int count = (len < 32767) ? len : 32767;
+                descriptor[d].SRCADDR.reg       = (uint32_t)&onePixelBuf;
+                descriptor[d].BTCTRL.bit.SRCINC = 0;
+                descriptor[d].BTCNT.reg         = count * 2;
+                descriptor[d].DESCADDR.reg      = (uint32_t)&descriptor[d+1];
+                len -= count;
+            }
+            descriptor[d-1].DESCADDR.reg        = 0;
+        } else {
+            // If high and low bytes are distinct, it's necessary to fill
+            // a buffer with pixel data (swapping high and low bytes because
+            // TFT and SAMD are different endianisms) and create a longer
+            // descriptor list pointing repeatedly to this data. We can do
+            // this slightly faster working 2 pixels (32 bits) at a time.
+            uint32_t *pixelPtr  = (uint32_t *)pixelBuf[0],
+                      twoPixels = __builtin_bswap16(color) * 0x00010001;
+            // We can avoid some or all of the buffer-filling if the color
+            // is the same as last time...
+            if(color == lastFillColor) {
+                // If length is longer than prior instance, fill only the
+                // additional pixels in the buffer and update lastFillLen.
+                if(len > lastFillLen) {
+                    int fillStart = lastFillLen / 2,
+                        fillEnd   = (((len < maxFillLen) ?
+                                       len : maxFillLen) + 1) / 2;
+                    for(i=fillStart; i<fillEnd; i++) pixelPtr[i] = twoPixels;
+                    lastFillLen = fillEnd * 2;
+                } // else do nothing, don't set pixels or change lastFillLen
+            } else {
+                int fillEnd = (((len < maxFillLen) ?
+                                 len : maxFillLen) + 1) / 2;
+                for(i=0; i<fillEnd; i++) pixelPtr[i] = twoPixels;
+                lastFillLen   = fillEnd * 2;
+                lastFillColor = color;
+            }
+
+            numDescriptors = (len + maxFillLen - 1) / maxFillLen;
+            for(d=0; d<numDescriptors; d++) {
+                int pixels = (len < maxFillLen) ? len : maxFillLen,
+                    bytes  = pixels * 2;
+                descriptor[d].SRCADDR.reg       = (uint32_t)pixelPtr + bytes;
+                descriptor[d].BTCTRL.bit.SRCINC = 1;
+                descriptor[d].BTCNT.reg         = bytes;
+                descriptor[d].DESCADDR.reg      = (uint32_t)&descriptor[d+1];
+                len -= pixels;
+            }
+            descriptor[d-1].DESCADDR.reg        = 0;
+        }
+        memcpy(dptr, &descriptor[0], sizeof(DmacDescriptor));
+
+        dma_busy = true;
+        dma.startJob();
+        while(dma_busy); // Wait for completion
+#ifdef __SAMD51__
+        // SAMD51: SPI DMA seems to leave the SPI peripheral in a freaky
+        // state on completion. Workaround is to explicitly set it back...
+        _spi->setDataMode(SPI_MODE0);
+#endif
+
+        // Unfortunately blocking is necessary. An earlier version returned
+        // immediately and checked dma_busy on startWrite() instead, but it
+        // turns out to be MUCH slower on many graphics operations (as when
+        // drawing lines, pixel-by-pixel), perhaps because it's a volatile
+        // type and doesn't cache. Working on this.
+
+#else // Non-DMA
+
+  #ifdef SPI_HAS_WRITE_PIXELS
+        #define TMPBUF_LONGWORDS (SPI_MAX_PIXELS_AT_ONCE + 1) / 2
+        #define TMPBUF_PIXELS    (TMPBUF_LONGWORDS * 2)
+        static uint32_t temp[TMPBUF_LONGWORDS];
+        uint32_t        c32    = color * 0x00010001;
+        uint16_t        bufLen = (len < TMPBUF_PIXELS) ? len : TMPBUF_PIXELS,
+                        xferLen, fillLen;
+
+        // Fill temp buffer 32 bits at a time
+        fillLen = (bufLen + 1) / 2; // Round up to next 32-bit boundary
+        for(uint32_t t=0; t<fillLen; t++) {
+            temp[t] = c32;
+        }
+
+        // Issue pixels in blocks from temp buffer
+        while(len) {                                 // While pixels remain
+            xferLen = (bufLen < len) ? bufLen : len; // How many this pass?
+            writePixels((uint16_t *)temp, xferLen);
+            len -= xferLen;
+        }
+  #else
+        while(len--) {
             HSPI_WRITE(hi);
             HSPI_WRITE(lo);
         }
-        return;
+  #endif
+
+#endif // end non-DMA
+
+    } else { // Bitbang SPI
+        while(len--) {
+            spiWrite(hi);
+            spiWrite(lo);
+        }
     }
-    for (uint32_t t=len; t; t--){
-        spiWrite(hi);
-        spiWrite(lo);
-    }
-#endif
 }
 
 /**************************************************************************/
@@ -411,9 +673,8 @@ void Adafruit_SPITFT::writeFillRect(int16_t x, int16_t y, int16_t w, int16_t h, 
     if(x2 >= _width)  w = _width  - x;
     if(y2 >= _height) h = _height - y;
 
-    int32_t len = (int32_t)w * h;
     setAddrWindow(x, y, w, h);
-    writeColor(color, len);
+    writeColor(color, (int32_t)w * h);
 }
 
 /**************************************************************************/
@@ -444,16 +705,21 @@ void inline Adafruit_SPITFT::writeFastHLine(int16_t x, int16_t y, int16_t w, uin
 
 /**************************************************************************/
 /*!
-   @brief    Draw a pixel - sets up transaction
-    @param   x   x coordinate
-    @param   y   y coordinate
-   @param    color 16-bit 5-6-5 Color to draw with
+    @brief  Draw a pixel - sets up transaction
+    @param  x  x coordinate
+    @param  y  y coordinate
+    @param  color 16-bit 5-6-5 Color to draw with
 */
 /**************************************************************************/
 void Adafruit_SPITFT::drawPixel(int16_t x, int16_t y, uint16_t color){
-    startWrite();
-    writePixel(x, y, color);
-    endWrite();
+    // Clip first...
+    if((x >= 0) && (x < _width) && (y >= 0) && (y < _height)) {
+        // THEN set up transaction (if needed) and draw...
+        startWrite();
+        setAddrWindow(x, y, 1, 1);
+        writePixel(color);
+        endWrite();
+    }
 }
 
 /**************************************************************************/
@@ -571,3 +837,4 @@ void Adafruit_SPITFT::drawRGBBitmap(int16_t x, int16_t y,
 }
 
 #endif // !__AVR_ATtiny85__
+
--- _MISC/_Sketchbook/libraries/Adafruit_GFX_Library/Adafruit_SPITFT.h
+++ _MISC/_Sketchbook/libraries/Adafruit_GFX_Library/Adafruit_SPITFT.h
@@ -12,7 +12,21 @@
 #include <SPI.h>
 #include "Adafruit_GFX.h"
 
-#define USE_FAST_PINIO
+#define USE_FAST_PINIO ///< If set, use PORT access instead of digitalWrite()
+//#define USE_SPI_DMA    ///< If set, use SPI DMA if available
+// If DMA is enabled, Arduino sketch MUST #include <Adafruit_ZeroDMA.h>
+// Estimated RAM usage:
+// 4 bytes/pixel on display major axis + 8 bytes/pixel on minor axis,
+// e.g. 320x240 pixels = 320 * 4 + 240 * 8 = 3,200 bytes.
+
+#if !defined(ARDUINO_ARCH_SAMD)
+ #undef USE_SPI_DMA ///< Only for SAMD chips
+#endif
+
+#ifdef USE_SPI_DMA
+ #pragma message ("SPI DMA IS ENABLED. HIGHLY EXPERIMENTAL.")
+ #include <Adafruit_ZeroDMA.h>
+#endif
 
 #if defined(__AVR__)
   typedef volatile uint8_t RwReg;
@@ -35,7 +49,6 @@
 
 /// A heavily optimized SPI display subclass of GFX. Manages SPI bitbanging, transactions, DMA, etc! Despite being called SPITFT, the classic SPI data/command interface is also used by OLEDs.
 class Adafruit_SPITFT : public Adafruit_GFX {
-    protected:
 
     public:
         Adafruit_SPITFT(uint16_t w, uint16_t h, int8_t _CS, int8_t _DC, int8_t _MOSI, int8_t _SCLK, int8_t _RST = -1, int8_t _MISO = -1);
@@ -88,6 +101,9 @@ class Adafruit_SPITFT : public Adafruit_GFX {
 	void      invertDisplay(boolean i);
 
         uint16_t  color565(uint8_t r, uint8_t g, uint8_t b);
+        void      writeCommand(uint8_t cmd);
+        void      spiWrite(uint8_t v);
+        uint8_t   spiRead(void);
 
     protected:
 	SPIClass *_spi;         ///< The SPI device we want to use (set in constructor)
@@ -116,14 +132,21 @@ class Adafruit_SPITFT : public Adafruit_GFX {
 	  dcpinmask;            ///< bitmask for turning on/off DC with fast register bitbang IO 
 #endif
 
-        void        writeCommand(uint8_t cmd);
-        void        spiWrite(uint8_t v);
-        uint8_t     spiRead(void);
-
 	uint8_t   invertOnCommand = 0,    ///<  SPI command byte to turn on invert
 	  invertOffCommand = 0;           ///<  SPI command byte to turn off invert
 	int16_t   _xstart = 0;   ///< Many displays don't have pixels starting at (0,0) of the internal framebuffer, this is the x offset from 0 to align
 	int16_t   _ystart = 0;   ///< Many displays don't have pixels starting at (0,0) of the internal framebuffer, this is the y offset from 0 to align
+
+#ifdef USE_SPI_DMA
+        Adafruit_ZeroDMA dma;                  ///< DMA instance
+        DmacDescriptor  *dptr          = NULL; ///< 1st descriptor
+        DmacDescriptor  *descriptor    = NULL; ///< Allocated descriptor list
+        uint16_t        *pixelBuf[2];          ///< Working buffers
+        uint16_t         maxFillLen;           ///< Max pixels per DMA xfer
+        uint16_t         lastFillColor = 0;    ///< Last color used w/fill
+        uint32_t         lastFillLen   = 0;    ///< # of pixels w/last fill
+        uint8_t          onePixelBuf;          ///< For hi==lo fill
+#endif
 };
 
 #endif // !__AVR_ATtiny85__
